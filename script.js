bd47cde6c9e3ac4bb815d23524278a85d9e5e380
heyhiru <hirentimbadiya74@gmail.com>
2022-11-28 10:42:41 +0530

    Added border radius to image

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const getNewDog = () => {
 fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
     .then(json => {
-            dogImageDiv.innerHTML = `<img src='${json.message}' height=100% width=100%/>`
+            dogImageDiv.innerHTML = `<img src='${json.message}' height=100% width=100% style = "border-radius : 5%"/>`
         })
 }
 
